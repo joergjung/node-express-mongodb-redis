d0b7f36e1e91f01dce1fadc4a8c980b8dbcee399
Joerg Jung <joerg-jung@web.de>
2016-03-31 01:28:17 +0200

    setup project and server

---
+++ index.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+// callback function has request and response object
+http.createServer(function(req, res) {
+    // write Header (Staus 200)
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'        
+    });
+    res.end('Hello world\n');    
+}).listen(3000, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:3000');
