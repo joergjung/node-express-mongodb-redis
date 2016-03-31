f076ed0fdd828bd1bbee8d62af38d4be25759382
Joerg Jung <joerg-jung@web.de>
2016-03-31 12:44:57 +0200

    add gitignore

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ http.createServer(function(req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'        
     });
-    res.end('Hello world\n');    
+    res.end('Hello world!!\n');    
 }).listen(3000, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:3000');
