1e92a28c6d7a39dfba861f67cf0bb9407fbfdd37
Joerg Jung <joerg-jung@web.de>
2016-04-01 13:01:15 +0200

    add basic CRUD functionality

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express');
-
 var app = express();
 
-app.get('/', function(req, res) {
-//    res.send('Hello World!!'); 
-   res.json({hello: 'hi'}); 
-});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// pass app as a function parameter
+var cats = require('./cats.js')(app);
 
 var server = app.listen(3000, function() {
     console.log("Server running at localhost:3000");    
