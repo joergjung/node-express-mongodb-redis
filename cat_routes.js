f5afa0b5d4461809307dc8cfa03b59171dd24c67
Joerg Jung <joerg-jung@web.de>
2016-04-02 16:30:41 +0200

    add model and routes

---
+++ cat_routes.js
@@ -0,0 +1,69 @@
+var _ = require('lodash');
+var Cat = require('./cat_model.js');
+
+module.exports = function(app) {
+        
+    /* --- Create --- */
+    app.post('/cat', function(req, res) {
+        var newCat = new Cat(req.body);
+        newCat.save(function(err) {
+            if (err) {
+                res.json({info: 'error creating cat', error: err});
+            }
+            res.json({info: 'cat created successfully'});
+        });
+    });
+    
+    /* --- Read --- */
+    app.get('/cat', function(req, res) {
+        Cat.find(function(err, cats) {
+            if (err) {
+                res.json({info: 'error finding cats'});
+            }
+            res.json({info: 'cats found successfully', data: cats});
+        });
+    });
+    
+    app.get('/cat/:id', function(req, res) {
+        Cat.findById(req.params.id, function(err, cat) {
+            if (err) {
+                res.json({info: 'error finding cat'});
+            }
+            if (cat) {
+                res.json({info: 'cat found successfully'});
+            } else {
+                res.json({info: 'cat not found'});
+            }
+        });
+    });
+    
+    /* --- Update --- */
+    app.put('/cat/:id', function(req, res) {
+        Cat.findById(req.params.id, function(err, cat) {
+            if (err) {
+                res.json({info: 'erros finding cat', error: err});
+            }
+            if(cat) {
+                _.merge(cat, req.body);
+                cat.save(function(err) {
+                    if (err) {
+                        res.json({info: 'error during cat update'});
+                    }
+                    res.json({info: 'cat updated successfully'});
+                });
+            } else {
+                res.json({info: 'cat not found'});
+            }
+        });
+    });
+    
+    /* --- Delete --- */
+    app.delete('/cat/:id', function(req, res) {
+        Cat.findByIdAndRemove(req.params.id, function(err) {
+            if (err) {
+                res.json({info: 'error removing cat'});
+            }
+            res.json({info: 'cat removed successfully'});           
+        });
+    });
+};
--- index.js
+++ index.js
@@ -3,14 +3,17 @@ var app = express();
 
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cats');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
 // pass app as a function parameter
-var cats = require('./cats.js')(app);
+var catRoutes = require('./cat_routes.js')(app);
 
 var server = app.listen(3000, function() {
     console.log("Server running at localhost:3000");    
-})
+});
